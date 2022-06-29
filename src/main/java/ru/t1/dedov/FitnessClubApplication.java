cedec4b1d247457f43685cae6703459005d5b98a
Alexnder Dedov <dedov.al@bk.ru>
2022-06-29 15:08:27 +0400

    Initial commit

---
+++ src/main/java/ru/t1/dedov/FitnessClubApplication.java
@@ -0,0 +1,13 @@
+package ru.t1.dedov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessClubApplication.class, args);
+    }
+
+}
